5f529f3a9b61575b65250f1ca576def0d6468ae1
PS204coder <psulej10@gmail.com>
2026-03-26 12:10:17 +0100

    player control work in proggres

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 @onready var racket: Sprite2D = $Racket
@@ -6,8 +6,17 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animation.play("walk")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("right"):
+		position.x += 2
+	if Input.is_action_just_pressed("left"):
+		position.x += 2
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += 50 
+		
+	if is_on_floor() != true:
+		position.y += 1
