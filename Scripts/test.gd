c0332891dffb27a13963d18d0f3a4145d9f84783
derry21378 <domi24domi@gmail.com>
2026-05-14 12:21:51 +0200

    questionably looking, but working menu added

--- Scripts/test.gd
+++ Scripts/test.gd
@@ -9,9 +9,13 @@ func _on_menu_button_pressed() -> void:
 	open_menu.emit()
 
 
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_continue_button_pressed() -> void:
 	menu.hide()
+
+
+func _on_options_button_pressed() -> void:
+	pass
+	
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
